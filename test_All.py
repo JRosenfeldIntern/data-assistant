6aa39c90219387b4f7d00daaa556f63932d87712
JRosenfeldIntern <JRosenfeld@esri.com>
2017-07-18 15:20:12 -0700

    Added Replace Syntax
    Added Replace framework into the test_all script

--- test_All.py
+++ test_All.py
@@ -6,16 +6,18 @@ from inc_datasources import _daGPTools, _dbConnStr, _configMatrix, _validConfigF
     _invalidConfigFiles, _localOutputPath, _localWorkspace
 
 sys.path.insert(0, _daGPTools)
-import test_CreateConfigFile, test_dlaPreview, test_dlaStage, test_dlaAppend
+import test_CreateConfigFile, test_dlaPreview, test_dlaStage, test_dlaAppend, test_dlaReplace
 
 if __name__ == '__main__':
     createConfig = test_CreateConfigFile.TestCreateConfigWorkflows()
     preview = test_dlaPreview.TestPreview()
     stage = test_dlaStage.TestStaging()
     append = test_dlaAppend.TestAppend()
+    replace = test_dlaReplace.TestReplace()
 
     for testCase,lw in zip(_configMatrix,_localWorkspace):
         createConfig.run_test(testCase,lw)
         preview.run_test(testCase,lw)
         stage.run_test(testCase,lw)
         append.run_test(testCase,lw)
+
